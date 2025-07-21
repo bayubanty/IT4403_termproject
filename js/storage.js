a90fe9ce1b9c335077f1ae3151c2eef0bc03a0c5
bayubanty <98555489+bayubanty@users.noreply.github.com>
2025-07-21 14:47:44 -0400

    Update storage.js

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,36 @@
+function getFavorites() {
+    return JSON.parse(localStorage.getItem('favorites')) || [];
+}
+
+function addFavorite(id) {
+    const list = getFavorites();
+    if (!list.includes(id)) {
+        list.push(id);
+        localStorage.setItem('favorites', JSON.stringify(list));
+    }
+}
+
+function removeFavorite(id) {
+    let list = getFavorites();
+    list = list.filter(item => item !== id);
+    localStorage.setItem('favorites', JSON.stringify(list));
+}
+
+function getWatchlist() {
+    return JSON.parse(localStorage.getItem('watchlist')) || [];
+}
+
+function addToWatchlist(id) {
+    const list = getWatchlist();
+    if (!list.includes(id)) {
+        list.push(id);
+        localStorage.setItem('watchlist', JSON.stringify(list));
+    }
+}
+
+function removeFromWatchlist(id) {
+    let list = getWatchlist();
+    list = list.filter(item => item !== id);
+    localStorage.setItem('watchlist', JSON.stringify(list));
+}
 
